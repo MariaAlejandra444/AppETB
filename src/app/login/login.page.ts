2ff2102652105b7c1a42b7f2bb9bc9bbda834073
mariaalejandra.varon <m_aaleja@hotmail.com>
2022-12-11 20:26:27 -0500

    validación servicio login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,7 +4,11 @@ import {
   FormControl,
   Validators,
   FormBuilder
-} from '@angular/forms'
+} from '@angular/forms';
+import { RestService } from '../services/rest.service'
+import { RouterLink, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -12,12 +16,17 @@ import {
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-
+  Items:any;
   ionicForm: FormGroup;
 
-  constructor(public fb: FormBuilder) {
+  dataLogin = {
+  nombre: '', 
+  password: ''
+  }
+
+  constructor(public fb: FormBuilder,public proveedor : RestService, private router :Router,private alertController: AlertController) {
     this.ionicForm = this.fb.group({
       'nombre': new FormControl("",Validators.required),
       'password': new FormControl("",Validators.required)
@@ -25,11 +34,38 @@ export class LoginPage implements OnInit {
 
   }
 
-  submitForm() {
-    console.log(this.ionicForm.value)
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Usuario o contraseña incorrecta',
+      message: 'Debe registrarse',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 
-  ngOnInit() {
+  submitForm() {
+    this.dataLogin =this.ionicForm.value;
+    console.log('datalogin es: '+ this.dataLogin.nombre);
+    this.loadInfo();
+  }
+
+  loadInfo(){
+    this.proveedor.loadInfo(this.dataLogin).then(data=>{
+        this.Items = data;
+        console.log(this.Items.status.id);
+        if(this.Items.status.id== "ok"){
+          this.router.navigate(['/home1'])
+        }
+        else{
+          this.presentAlert();
+
+        }
+    }).catch(data=>{
+      console.log(data);
+
+    })
   }
 
 }
