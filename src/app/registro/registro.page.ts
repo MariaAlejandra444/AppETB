2065579d8fdefc007567c1efefad6a44d29c45da
mariaalejandra.varon <m_aaleja@hotmail.com>
2022-12-11 23:28:22 -0500

    modificación pantalla registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { RestService } from '../services/rest.service'
+import { RouterLink, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
+export class RegistroPage {
+  Items:any;
+  ionicForm: FormGroup;
 
-  constructor() { }
+  dataLogin = {
+  nombre:'',
+  apellido:'',
+  email:'',
+  contrasena:''
+  }
 
-  ngOnInit() {
+  constructor(public fb: FormBuilder) {
+    this.ionicForm = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'apellido': new FormControl("",Validators.required),
+      'email': new FormControl("",Validators.required),
+      'contrasena': new FormControl("",Validators.required)
+
+    })
+   }
+
+   submitForm() {
+    this.dataLogin =this.ionicForm.value;
+    console.log('datalogin es: '+ this.dataLogin.email);
   }
 
 }
