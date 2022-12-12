c54f8019c929e726cee75430d5a55b130c4b59ca
mariaalejandra.varon <m_aaleja@hotmail.com>
2022-12-11 23:40:50 -0500

    servicio de registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -5,7 +5,7 @@ import {
   Validators,
   FormBuilder
 } from '@angular/forms';
-import { RestService } from '../services/rest.service'
+import { RegistroService } from '../services/registro.service'
 import { RouterLink, RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
@@ -26,7 +26,7 @@ export class RegistroPage {
   contrasena:''
   }
 
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder,public proveedor : RegistroService,private router :Router,private alertController: AlertController) {
     this.ionicForm = this.fb.group({
       'nombre': new FormControl("",Validators.required),
       'apellido': new FormControl("",Validators.required),
@@ -39,6 +39,35 @@ export class RegistroPage {
    submitForm() {
     this.dataLogin =this.ionicForm.value;
     console.log('datalogin es: '+ this.dataLogin.email);
+    this.loadInfo();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Datos incompletos',
+      message: 'Debe Incluir toda la información',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  loadInfo(){
+    this.proveedor.loadInfo(this.dataLogin).then(data=>{
+        this.Items = data;
+        console.log(this.Items.status.id);
+        if(this.Items.status.id== "ok"){
+          this.router.navigate(['/home1'])
+        }
+        else{
+          this.presentAlert();
+
+        }
+    }).catch(data=>{
+      console.log(data);
+
+    })
   }
 
 }
